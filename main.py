74c557e65d6398f0c93fbf744168f2912dee910f
LianosIoannis <giannislianos.g@gmail.com>
2023-03-23 17:19:47 +0200

    aaa

--- main.py
+++ main.py
@@ -1,21 +1,39 @@
 import sys
 from prophet import Prophet
 import pandas as pd
-from pathlib import Path
-
-sales_filename = Path(sys.argv[1])
 
 
-df = pd.read_csv(sales_filename, sep=",", parse_dates=["ds"], dayfirst=True)
-
-m = Prophet()
-m.fit(df)
-future = m.make_future_dataframe(periods=30)
-forecast = m.predict(future)
-
-sales_filename_forecast = sales_filename.parent / (sales_filename.stem + "_forecast.csv")
-
-forecast.to_csv(sales_filename_forecast, index=False, columns=['ds', 'yhat'])
+def print_data(d, n):
+    pd.set_option('display.max_rows', None)
+    print(d.tail(n))
 
 
+def save_data(d, f):
+    d.to_csv(f, index=False)
 
+
+#filename = "C:\\Users\\User\\Desktop\\sales_day.csv"
+folder = "C:\\Users\\User\\Desktop\\"
+
+#horizon = int(sys.argv[1])
+#sales_file = folder + str((sys.argv[2]))
+#forecast_file = folder + str((sys.argv[3]))
+#frequency = str((sys.argv[4]))
+
+horizon = 15
+sales_file = folder + "sales_day.csv"
+forecast_file = folder + "forecast_day.csv"
+frequency = "D"
+
+data = pd.read_csv(sales_file, parse_dates=["ds"])
+
+model = Prophet()
+model.fit(data)
+
+future = model.make_future_dataframe(periods=horizon, freq=frequency, include_history=True)
+forecast = model.predict(future)
+
+forecast.rename(columns={'yhat': 'y'}, inplace=True)
+
+print_data(forecast[["ds", "y"]], horizon)
+save_data(forecast[["ds", "y"]].tail(horizon), forecast_file)
